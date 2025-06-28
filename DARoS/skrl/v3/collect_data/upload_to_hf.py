29f0401a123745855123438074ae3dfa1a0f60a9
Sangjun Park <06park.sangjun@gmail.com>
2025-06-28 10:18:59 -0400

    stuff

--- DARoS/skrl/v3/collect_data/upload_to_hf.py
+++
@@ -1,5 +0,0 @@
-from huggingface_hub import HfApi
-import os
-
-hub_api = HfApi()
-hub_api.create_tag("ssangjunpark/daros19_1856", tag="v2.0", repo_type="dataset")
--- DARoS/skrl_final/collect_data/upload_to_hf.py
+++
@@ -1,5 +0,0 @@
-from huggingface_hub import HfApi
-import os
-
-hub_api = HfApi()
-hub_api.create_tag("ssangjunpark/daros25_0541", tag="v2.0", repo_type="dataset")
